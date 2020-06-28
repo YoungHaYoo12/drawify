8dc1b1a3f394320decef6f2102c7a1d48f7d972d
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-28 04:41:42 +0000

    Add models to shell context.

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager, Shell
 from flask_migrate import Migrate
 from app import create_app, db
+from app.models import User, Drawing
 
 app = create_app('default')
 manager = Manager(app)
@@ -9,7 +10,9 @@ Migrate(app,db)
 
 def make_shell_context():
   return dict(app=app,
-              db=db)
+              db=db,
+              User=User,
+              Drawing=Drawing)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
