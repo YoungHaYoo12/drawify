820e0d2bf2dbfd15c0ba76b40c4c5796a6d5b7f1
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-28 00:35:54 +0000

    make shell context.

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_migrate import Migrate
 from app import create_app, db
 
 app = create_app('default')
 manager = Manager(app)
 Migrate(app,db)
+
+
+def make_shell_context():
+  return dict(app=app,
+              db=db)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
 
 if __name__ == '__main__':
   manager.run()
