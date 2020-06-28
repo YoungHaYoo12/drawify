a4408ad0580dd7a55fa3a4d84961afc86ec507b6
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-28 00:36:29 +0000

    Implement function to save an image given its data URL.

---
+++ app/images.py
@@ -0,0 +1,22 @@
+import secrets
+import os
+import io
+import base64
+from PIL import Image
+
+def save_image(dataURL):
+  # retrieve image
+  dataURL_cleaned = dataURL.split(',')[1]
+  img_data = io.BytesIO(base64.b64decode(dataURL_cleaned))
+  img = Image.open(img_data)
+
+  # create secure filepath
+  random_hex = secrets.token_hex(8)
+  picture_fn = random_hex + '.png'
+  app_root_path = os.path.dirname(os.path.abspath(__file__))
+  picture_path = os.path.join(app_root_path,'static','images',picture_fn)
+
+  # save image
+  img.save(picture_path)
+
+  return picture_fn
