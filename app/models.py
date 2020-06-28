b5f8b996934da1ef63ba5718a06e1b0032b666d3
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-28 02:37:38 +0000

    Add User and Drawing models.

---
+++ app/models.py
@@ -0,0 +1,41 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+  __tablename__ = 'users'
+  id = db.Column(db.Integer,primary_key=True)
+  username = db.Column(db.String(64),unique=True,index=True)
+  email = db.Column(db.String(64),unique=True,index=True)
+  password_hash = db.Column(db.String(128))
+  drawings = db.relationship('Drawing',backref='user',lazy='dynamic',cascade="all, delete-orphan")
+
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable attribute')
+  
+  @password.setter
+  def password(self,password):
+    self.password_hash = generate_password_hash(password)
+  
+  def verify_password(self,password):
+    return check_password_hash(self.password_hash,password)
+
+  def __init__(self,username,email,password):
+    self.username = username
+    self.email = email
+    self.password = password 
+  
+  def __repr__(self):
+    return f"<User {self.username}>"
+
+class Drawing(db.Model):
+  __tablename__ = 'drawings'
+  id = db.Column(db.Integer,primary_key=True)
+  filename = db.Column(db.String(64),unique=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+  def __init__(self,filename):
+    self.filename = filename
+  
+  def __repr__(self):
+    return f"<Drawing {self.filename}>"
