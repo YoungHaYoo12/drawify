da87ba68cc98c590b547af6520c7b89189b2b4f9
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-29 10:09:32 +0000

    Add Questions model.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 
 class User(db.Model,UserMixin):
@@ -10,6 +11,14 @@ class User(db.Model,UserMixin):
   email = db.Column(db.String(64),unique=True,index=True)
   password_hash = db.Column(db.String(128))
   drawings = db.relationship('Drawing',backref='user',lazy='dynamic',cascade="all, delete-orphan")
+
+  questions_sent = db.relationship('Question',foreign_keys='Question.sender_id',backref='author',lazy='dynamic')
+  questions_received = db.relationship('Question',foreign_keys='Question.recipient_id',backref='recipient',lazy='dynamic')
+  last_question_read_time = db.Column(db.DateTime)
+
+  def new_questions(self):
+    last_read_time = self.last_question_read_time or datetime(1900,1,1)
+    return Question.query.filter_by(recipient=self).filter(Question.timestamp > last_read_time).count()
 
   @property
   def password(self):
@@ -39,9 +48,22 @@ class Drawing(db.Model):
   id = db.Column(db.Integer,primary_key=True)
   filename = db.Column(db.String(64),unique=True)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  question = db.relationship('Question',backref='drawing',uselist=False)
 
   def __init__(self,filename):
     self.filename = filename
   
   def __repr__(self):
     return f"<Drawing {self.filename}>"
+
+class Question(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  sender_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  recipient_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  drawing_id = db.Column(db.Integer, db.ForeignKey('drawings.id'))
+  answer = db.Column(db.String(64))
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+  def __repr__(self):
+    return f"<Question {self.answer}>"
+
