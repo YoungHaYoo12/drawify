326d04442f10f912e71b56f166e7948a55d982f0
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-07-03 10:18:52 +0000

    Add number range validator to AddGameForm.

--- app/games/forms.py
+++ app/games/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,NumberRange
 
 class AddGameForm(FlaskForm):
-  max_points = IntegerField('Max Points',validators=[DataRequired()])
+  max_points = IntegerField('Max Points',validators=[DataRequired(),NumberRange(min=1, max=20, message='Max Points Not In Valid Number Range')])
   submit = SubmitField('Create Game')
