68cb9d16aab0635f8135e7277881437005f08ef6
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-07-04 08:54:09 +0000

    Set up unit testing.

--- manage.py
+++ manage.py
@@ -2,11 +2,42 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Drawing,Question,Notification,Game
+import os
 
 app = create_app('default')
 manager = Manager(app)
 Migrate(app,db)
 
+########################
+#Coverage SetUp
+if os.environ.get('FLASK_COVERAGE'):
+  import coverage
+  COV = coverage.coverage(branch=True,include='app/*')
+  COV.start()
+########################
+
+@manager.command
+def test(coverage=False):
+  """Run the Unit Tests"""
+  if coverage and not os.environ.get('FLASK_COVERAGE'):
+    import sys 
+    os.environ['FLASK_COVERAGE'] = '1'
+    os.execvp(sys.executable, [sys.executable] + sys.argv)
+
+  import unittest
+  tests = unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+
+  if COV:
+    COV.stop()
+    COV.save()
+    print('Coverage Summary:')
+    COV.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir,'tmp/coverage')
+    COV.html_report(directory=covdir)
+    print('HTML version: file://%s/index.html'%covdir)
+    COV.erase()
 
 def make_shell_context():
   return dict(app=app,
