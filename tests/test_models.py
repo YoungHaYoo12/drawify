5df38b69ab6af33a2a5ec31960fd2a6de2960b00
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-07-04 13:52:14 +0000

    Add additional unit tests for User and Guest models.

--- tests/test_models.py
+++ tests/test_models.py
@@ -92,6 +92,60 @@ class UserModelTestCase(FlaskTestCase):
       db.session.add(u2)
       db.session.commit()
   
+  def test_user_questions_methods(self):
+    u1 = User(email='one@one.com', username='one', password='one')
+    db.session.add(u1)
+    db.session.commit()
+    
+    # new_questions() and new_questions_content()
+    q1 = Question(answer='q1')
+    q1.recipient = u1
+    db.session.add(q1)
+    db.session.commit()
+    u1.last_question_read_time = datetime.utcnow()
+    db.session.commit()
+    self.assertEqual(u1.new_questions(),0)
+    self.assertFalse(q1 in u1.new_questions_content())
+
+    q2 = Question(answer='q2')
+    q2.recipient = u1
+    db.session.add(q2)
+    db.session.commit()
+    self.assertEqual(u1.new_questions(),1)
+    self.assertTrue(q2 in u1.new_questions_content())
+
+  def test_user_games_methods(self):
+    u1 = User(email='one@one.com', username='one', password='one')
+    u2 = User(email='two@two.com', username='two', password='two')
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.commit()
+
+    # unconfirmed_games() and unconfirmed_games_count()
+    self.assertEqual(u1.unconfirmed_games_count(),0)
+    game1 = Game(guest=u1,max_points=7)
+    db.session.add(game1)
+    db.session.commit()
+    self.assertEqual(u1.unconfirmed_games_count(),1)
+    self.assertTrue(game1 in u1.unconfirmed_games())
+
+    # unanswered_games() and unanswered_games_count()
+    self.assertEqual(u1.unanswered_games_count(),0)
+    game2 = Game(author=u2,guest=u1,turn='waiting_guest_answer',max_points=7)
+    game3 = Game(author=u1,guest=u2,turn='waiting_author_answer',max_points=7)
+    game4 = Game(author=u2,guest=u1,turn='waiting_author_answer',max_points=7)
+    game5 = Game(author=u1,guest=u2,turn='waiting_guest_answer',max_points=7)
+    db.session.add_all([game2,game3,game4,game5])
+    db.session.commit()
+    self.assertEqual(u1.unanswered_games_count(),2)
+    self.assertTrue(game2 in u1.unanswered_games())
+    self.assertTrue(game3 in u1.unanswered_games())
+    self.assertFalse(game4 in u1.unanswered_games())
+    self.assertFalse(game5 in u1.unanswered_games())
+
+    # test all games 
+    self.assertEqual(u1.all_games().count(),5)
+
   def test_friendship_functionality(self):
     u1 = User(email='one@one.com', username='one', password='one')
     u2 = User(email='two@two.com',username='two',password='two')
@@ -198,13 +252,48 @@ class GameModelTestCase(FlaskTestCase):
     self.assertEqual(game.turn,'author')
 
   def test_methods(self):
+    u1 = User(username='one',email='one@one.com',password='one')
+    u2 = User(username='two',email='two@two.com',password='two')
     game = Game(
       current_author_points=7,
       current_guest_points=8,
-      max_points=8
+      max_points=8,
+      author=u1,
+      guest=u2,
+      turn='author'
     )
+    # checking wins
     self.assertFalse(game.is_author_win())
     self.assertTrue(game.is_guest_win())
+    self.assertFalse(game.is_user_win(u1))
+    self.assertTrue(game.is_user_loss(u1))
+    self.assertTrue(game.is_user_win(u2))
+    self.assertFalse(game.is_user_loss(u2))
+
+    # turn-related methods
+    self.assertTrue(game.is_turn(u1))
+    self.assertFalse(game.is_turn(u2))
+    game.make_user_turn(u2)
+    self.assertFalse(game.is_turn(u1))
+    self.assertTrue(game.is_turn(u2))
+    game.turn = 'waiting_author_answer'
+    self.assertTrue(game.is_waiting_answer_from(u1))
+    self.assertFalse(game.is_waiting_answer_from(u2))
+
+    # update score 
+    game.update_user_score(u1,10)
+    self.assertEqual(game.current_author_points,17)
+
+    # user validation methods
+    self.assertTrue(game.is_author(u1))
+    self.assertFalse(game.is_author(u2))
+
+    u3 = User(username='three',email='three@three.com',password='three')
+    self.assertTrue(game.validate_players(u1,u2))
+    self.assertFalse(game.validate_players(u1,u3))
+
+    self.assertEqual(game.get_opponent(u1),u2)
+    self.assertEqual(game.get_opponent(u2),u1)
 
   def test_repr(self):
     game = Game()
